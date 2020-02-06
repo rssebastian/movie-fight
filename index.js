5af1a611a7f192fcfa08676a890c20d5fc95e956
EC2 Default User <ec2-user@ip-172-31-18-226.us-west-2.compute.internal>
2020-02-06 23:55:08 +0000

    dropdown menu working, migrated autocomplete widget to js instead of html

--- index.js
+++ index.js
@@ -11,18 +11,36 @@ const fetchData = async (searchTerm) => {
     return response.data.Search;
 };
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+    <label><b>Search for a movie!</b></label>
+    <input />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+          <div class="dropdown-content results">
+          </div>
+        </div>
+    </div>
+`;
+
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
 
 const onInput = async event => {
     const movies = await fetchData(event.target.value);
+    resultsWrapper.innerHTML = '';
+    dropdown.classList.add('is-active');
     for (let movie of movies) {
-        const div = document.createElement('div');
-        div.innerHTML = `
-            <img src="${movie.Poster}"/>
-            <h1>${movie.Title}</h1>
+        const option = document.createElement('a');
+        const imgSrc = movie.Poster === "N/A" ? '' : movie.Poster;
+        option.innerHTML = `
+            <img src="${imgSrc}"/>
+            ${movie.Title}
         `;
-        
-        document.querySelector('#target').appendChild(div);
+        option.classList.add('dropdown-item');
+        resultsWrapper.appendChild(option);
     };
 };
 

d90ae4b7146d7df2dd0d6528ccb2dceada445eca
EC2 Default User <ec2-user@ip-172-31-18-226.us-west-2.compute.internal>
2020-02-06 18:38:53 +0000

    added text input field to fetch movie data

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+    const response = await axios.get("https://www.omdbapi.com/", {
+        params: {
+            apikey: 'afcfd597',
+            s: searchTerm
+        }
+    });
+    
+    console.log(response.data);
+};
+
+const input = document.querySelector('input');
+let timeoutId;
+const onInput = event => {
+    if (timeoutId) {
+        clearTimeout(timeoutId);
+    };
+    setTimeout(() => {
+        fetchData(event.target.value)
+    }, 1000);
+    
+};
+input.addEventListener('input', onInput);

5e341f130bc8d820bb6a94a9413a443e5d638865
EC2 Default User <ec2-user@ip-172-31-18-226.us-west-2.compute.internal>
2020-02-08 18:01:36 +0000

    refactored rendering logic, can now pass a customizable renderOption method in to autoComplete generator

--- autocomplete.js
+++ autocomplete.js
@@ -1,4 +1,4 @@
-const createAutoComplete = ({ root }) => {
+const createAutoComplete = ({ root, renderOption }) => {
     root.innerHTML = `
         <label><b>Search for a movie!</b></label>
         <input class="input"/>
@@ -25,11 +25,8 @@ const createAutoComplete = ({ root }) => {
         dropdown.classList.add('is-active');
         for (let movie of movies) {
             const option = document.createElement('a');
-            const imgSrc = movie.Poster === "N/A" ? '' : movie.Poster;
-            option.innerHTML = `
-                <img src="${imgSrc}"/>
-                ${movie.Title}
-            `;
+            
+            option.innerHTML = renderOption(movie);
             option.classList.add('dropdown-item');
             option.addEventListener('click', () => {
                 dropdown.classList.remove('is-active');
